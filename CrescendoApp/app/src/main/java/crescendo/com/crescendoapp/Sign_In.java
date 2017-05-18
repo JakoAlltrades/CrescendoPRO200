a8e6a54f2325878b5d8daa1011fe8d892767fc0e
BrandonCoats <bgcoats1313@gmail.com>
2017-05-18 13:26:20 -0600

    App
    I added the sign in as a test

---
+++ CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/Sign_In.java
@@ -0,0 +1,17 @@
+package crescendo.com.crescendoapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Brandon on 5/18/2017.
+ */
+
+public class Sign_In  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    } 
+}
