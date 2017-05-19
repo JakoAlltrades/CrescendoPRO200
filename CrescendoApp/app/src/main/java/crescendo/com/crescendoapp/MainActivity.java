d6a1cb594826df29f768990608a8a59c8dc13831
JakoAlltrades <Jpriemo@gmail.com>
2017-05-19 12:42:13 -0600

    Revert "The great Copypasta"
    This reverts commit 5c11b0f8e17b88fde2166b43b222bb5cb1e241cd.

--- CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/MainActivity.java
+++ CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/MainActivity.java
@@ -1,9 +1,7 @@
 package crescendo.com.crescendoapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void signIn(View view)
-    {
-        Intent myIntent = new Intent(MainActivity.this, Sign_In.class);
-        startActivity(myIntent);
-    }
-    public void signUp(View view)
-    {
-        Intent myIntent = new Intent(MainActivity.this, Sign_Up.class);
-        startActivity(myIntent);
-    }
-
 }
