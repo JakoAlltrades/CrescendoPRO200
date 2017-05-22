3655268f86954cecccc6aeaab7ca5fd751b69e3b
JakoAlltrades <Jpriemo@gmail.com>
2017-05-22 14:53:59 -0600

    DBHandler Commit
    It isn't working yet but is close

--- CrescendoSite/CrescendoWebsite/Controllers/HomeController.cs
+++ CrescendoSite/CrescendoWebsite/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CrescendoWebsite.Models;
+
 
 namespace CrescendoWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        DBhandler db = new DBhandler();
         // GET: Home
         public ActionResult Index()
         {
---
+++ CrescendoSite/CrescendoWebsite/Models/DBhandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Intuit.QuickBase.Client;
+using Intuit.QuickBase.Core;
+
+namespace CrescendoWebsite.Models
+{
+    public class DBhandler
+    {
+        public DBhandler()
+        {
+            IQClient client = QuickBase.Login("Jpriemo1234@gmail.com", "Crescendo1", "johnpriem.quickbase.com");
+            IQApplication application = client.Connect("bms24ys95");
+            IQTable table =  application.GetTable("bms24ytdy");
+
+            foreach(var record in table.Records)
+            {
+                Console.WriteLine(record[0]);
+            }
+        }
+    }
+}
