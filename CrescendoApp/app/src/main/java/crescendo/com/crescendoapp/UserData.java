b82315d0f213561d77f9171f928b4bf434da1606
BrandonCoats <bgcoats1313@gmail.com>
2017-05-22 13:00:16 -0600

    Fixed intent routing
    bug fixes

--- CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/UserData.java
+++ CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/UserData.java
@@ -1,7 +1,9 @@
 package crescendo.com.crescendoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Brandon on 5/18/2017.
@@ -13,5 +15,15 @@ public class UserData  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userdata);
+    }
+    public void viewRecordings(View view)
+    {
+        Intent myIntent = new Intent(UserData.this, ViewRecordings.class);
+        startActivity(myIntent);
+    }
+    public void viewTunings(View view)
+    {
+        Intent myIntent = new Intent(UserData.this, viewtune.class);
+        startActivity(myIntent);
     }
 }
--- CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/UserHome.java
+++ CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/UserHome.java
@@ -1,7 +1,9 @@
 package crescendo.com.crescendoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Brandon on 5/18/2017.
@@ -13,5 +15,35 @@ public class UserHome  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userhome);
+    }
+    public void view(View view)
+    {
+        Intent myIntent = new Intent(UserHome.this, UserData.class);
+        startActivity(myIntent);
+    }
+    public void record(View view)
+    {
+        Intent myIntent = new Intent(UserHome.this, Record.class);
+        startActivity(myIntent);
+    }
+    public void tune(View view)
+    {
+        Intent myIntent = new Intent(UserHome.this, Tune.class);
+        startActivity(myIntent);
+    }
+    public void metronome(View view)
+    {
+        Intent myIntent = new Intent(UserHome.this, Metronome.class);
+        startActivity(myIntent);
+    }
+    public void chords(View view)
+    {
+        Intent myIntent = new Intent(UserHome.this, Chords.class);
+        startActivity(myIntent);
+    }
+    public void scales(View view)
+    {
+        Intent myIntent = new Intent(UserHome.this, Scales.class);
+        startActivity(myIntent);
     }
 }
