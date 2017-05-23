c42e46d106e5a250391fb180084c15e38b90152c
JakoAlltrades <Jpriemo@gmail.com>
2017-05-23 14:40:17 -0600

    Created User and DB userFunctions
    User can log in, be created and added to database

--- CrescendoSite/CrescendoWebsite/Models/DBhandler.cs
+++ CrescendoSite/CrescendoWebsite/Models/DBhandler.cs
@@ -9,15 +9,97 @@ namespace CrescendoWebsite.Models
 {
     public class DBhandler
     {
+        IQClient client;
+        IQApplication application;
+        IQTable table;
+
+        Dictionary<String, String> tableNames = new Dictionary<String, String> { {"Users", "bms24ytdy" }, { "Recordings", "bms24ytgg" } };
+
+        private int curUserID;//handles the current UserID for the database
         public DBhandler()
         {
-            IQClient client = QuickBase.Login("Jpriemo1234@gmail.com", "Crescendo1", "johnpriem.quickbase.com");
-            IQApplication application = client.Connect("bms24ys95", "duzpt2fcvsybbgkrkup4bjurh8b");
-            IQTable table =  application.GetTable("bms24ytdy");
-
-            foreach(var record in table.Records)
+            client = QuickBase.Login("Jpriemo1234@gmail.com", "Crescendo1", "johnpriem.quickbase.com");
+            application = client.Connect("bms24ys95", "duzpt2fcvsybbgkrkup4bjurh8b");
+            table = application.GetTable(GetTableID("Users"));
+            setCurUserID();
+            /*Pulls all users and prints their information
+            foreach(IQRecord record in table.Records)
             {
-                Console.WriteLine(record[0]);
+                Console.WriteLine(record[0] + "- " + record[1] + ":" + record[2]);
+            }*/
+
+            //CreateUser("Jack", "Priem");
+        }
+
+        private string GetTableID(string TableName)
+        {
+            return tableNames.Where(x => x.Key == "Users").Select(x => x.Value).SingleOrDefault();
+        }
+
+        public bool CreateUser(String userName, String password)
+        {
+            //check the passwords before calling method
+            table.Query();
+            bool userCreated = false;
+            IQRecord tempUser = table.Records.Where(x => x[1] == userName).SingleOrDefault();
+            if(tempUser == null)
+            {
+                tempUser = table.NewRecord();
+                tempUser[0] = curUserID.ToString();
+                tempUser[1] = userName;
+                tempUser[2] = password;
+                table.Records.Add(tempUser);
+                table.AcceptChanges();
+                userCreated = true;
+            }
+            else
+            {
+                //Error username exists
+            }
+            return userCreated;
+        }
+
+        public bool SignIn(String userName, String password)
+        {
+            table.Query();
+            bool userLoggedIn = false;
+            IQRecord prevUser = table.Records.Where(x => x[1] == userName).SingleOrDefault();
+            if(prevUser != null)
+            {
+                if(prevUser[2] == password)
+                {
+                    userLoggedIn = true;
+                }
+                else
+                {
+                    //User found wrong password
+                }
+            }
+            else
+            {
+                //User not found, make sure that person entered their name correctly
+            }
+            return userLoggedIn;
+        }
+
+        private void IncrementCurUserID()
+        {
+            curUserID++;
+        }
+
+        private void setCurUserID()
+        {
+            table.Query();
+            string lastID = "0";
+            if (table.Records.Count > 0)
+            { 
+                lastID = table.Records.OrderByDescending(x => x[0]).Select(x => x[0]).First();
+                table.Records.OrderBy(x => x[0]);
+            }
+            if(Int32.TryParse(lastID, out curUserID))
+            {
+                IncrementCurUserID();
+                //Console.WriteLine("Current ID set to: " + curUserID);
             }
         }
     }
---
+++ CrescendoSite/CrescendoWebsite/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrescendoWebsite.Models
+{
+    public class User
+    {
+        int? UserID { get; set; } 
+        public string UserName { get; set; }
+        string UserPassword { get; set; }
+
+        public User(String userName, string userPassword)
+        {
+            UserID = null;
+            UserName = userName;
+            UserPassword = userPassword;
+        }
+
+        public User(int userID, string userName, string userPassword)
+        {
+            UserID = userID;
+            UserName = userName;
+            UserPassword = userPassword;
+        }
+    }
+}
