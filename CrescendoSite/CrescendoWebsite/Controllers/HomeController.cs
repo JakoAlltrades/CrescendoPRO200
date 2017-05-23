f6da234f23f33ea974a19a2b2f77028389edf5a3
Chance Hollis <chancehollis@gmail.com>
2017-05-23 13:37:51 -0600

    added action methods to controller

--- CrescendoSite/CrescendoWebsite/Controllers/HomeController.cs
+++ CrescendoSite/CrescendoWebsite/Controllers/HomeController.cs
@@ -11,10 +11,55 @@ namespace CrescendoWebsite.Controllers
     public class HomeController : Controller
     {
         DBhandler db = new DBhandler();
-        // GET: Home
+
         public ActionResult Index()
         {
             return View();
-        }        
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User u)
+        {
+            ViewResult vr = null;
+            vr = View("UserHome", u);
+            return vr;
+        }
+
+        public ActionResult NewUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewUser(User u)
+        {
+            ViewResult vr = null;
+            vr = View("UserHome", u);
+            return vr;
+        }
+
+        [HttpPost]
+        public ActionResult UserHome(User u)
+        {
+            ViewResult vr = View(u);
+            return vr;
+        }
+
+        [HttpPost]
+        public ActionResult UserData(User u)
+        {
+            ViewResult vr = View(u);
+            return vr;
+        }
+
+        [HttpPost]
+        public ActionResult Recordings(User u) // this should take a list<Recording> instead? or maybe user just has a list of Recordings?
+        {
+            ViewResult vr = View(u);
+            return vr;
+        }
     }
 }
