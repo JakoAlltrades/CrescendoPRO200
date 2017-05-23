d0d99aa598914915bf89505393cdf00c8c7be7ca
Chance Hollis <chancehollis@gmail.com>
2017-05-23 13:38:36 -0600

    Added some more views and a user model

---
+++ CrescendoSite/CrescendoWebsite/Models/Recording.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrescendoWebsite.Models
+{
+    public class Recording
+    {
+        public string RecordingName { get; set; }
+        public double RecordingLength { get; set; }
+    }
+}
