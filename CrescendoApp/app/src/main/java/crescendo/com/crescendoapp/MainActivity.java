5aad016c41249d32311cead76572af88779febef
BrandonCoats <bgcoats1313@gmail.com>
2017-05-24 14:53:04 -0600

    Major changes to the structure
    got rid of home and made sign in the home

--- CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/MainActivity.java
+++ CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/MainActivity.java
@@ -4,17 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sign_in);
     }
-    public void signIn(View view)
+    public void userHome(View view)
     {
-        Intent myIntent = new Intent(MainActivity.this, Sign_In.class);
+        EditText username = (EditText)findViewById(R.id.username);
+        EditText password = (EditText)findViewById(R.id.password);
+        /*
+        checkDatabase(username,password);
+         */
+        Intent myIntent = new Intent(MainActivity.this, UserHome.class);
         startActivity(myIntent);
     }
     public void signUp(View view)
