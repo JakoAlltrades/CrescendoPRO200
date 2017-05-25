6441c199a3d066d8e7171baa76e48a205fc3e670
JakoAlltrades <Jpriemo@gmail.com>
2017-05-25 11:11:07 -0600

    Java Quickbase Pluggins
    Big commit with a few classes added

---
+++ CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/DBHandler.java
@@ -0,0 +1,21 @@
+package crescendo.com.crescendoapp;
+
+import com.intuit.quickbase.api.QuickBase;
+import com.intuit.quickbase.api.QuickBaseAuthenticator;
+import com.intuit.quickbase.api.QuickBaseConnection;
+
+/**
+ * Created by jprirm on 5/25/2017.
+ */
+
+public class DBHandler {
+    QuickBaseConnection QBC;
+    QuickBase QB;
+    QuickBaseAuthenticator QBA;
+
+    public DBHandler()
+    {
+
+    }
+
+}
