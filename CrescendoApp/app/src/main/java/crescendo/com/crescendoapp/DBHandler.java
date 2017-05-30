95f320a24d20a8a9880f5fa17c56e846dfe8fefc
JakoAlltrades <Jpriemo@gmail.com>
2017-05-29 21:12:17 -0600

    DBhandler update

--- CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/DBHandler.java
+++ CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/DBHandler.java
@@ -1,7 +1,5 @@
 package crescendo.com.crescendoapp;
 
-import android.util.Log;
-
 import com.intuit.quickbase.api.QuickBase;
 import com.intuit.quickbase.api.QuickBaseAuthenticator;
 import com.intuit.quickbase.api.QuickBaseConnection;
@@ -19,17 +17,19 @@ public class DBHandler {
 
     public DBHandler() {
 
-        QBClient = new QuickBaseClient("Jpriemo1234@gmail.com", "Crescendo1", "https://johnpriem.quickbase.com/db/bms24ys95");
-        QBClient.setAppToken("bms24ys95");
+        //QBClient = new QuickBaseClient("Jpriemo1234@gmail.com", "Crescendo1", "https://johnpriem.quickbase.com/db/");
+        QBClient = new QuickBaseClient("Jpriemo1234@gmail.com", "Crescendo1", "https://johnpriem.quickbase.com/db/");
+        QBClient.setAppToken("duzpt2fcvsybbgkrkup4bjurh8b");
         GetsUserByID(1);
     }
 
     public void GetsUserByID(int id)
     {
         try {
-            String tableID = QBClient.findDbByName("CrescendoDB");
-            Log.i("Table:", tableID);
-            QBClient.doQuery("bms24ytdy", "{'0'.Ex.'" + id + "'}","UserID UserName UserPassword", "", "");
+            //String tableID = QBClient.findDbByName("CrescendoDB");
+            //Log.i("Table:", tableID);
+            QBClient.doQuery("bms24ytdy", "{'0'.EX." + id + "}","1", "", "");
+            //QBClient.doQuery("bms24ytdy", "{'0'.EX." + id + "}","UserID.UserName.UserPassword", "", "");
         }
         catch (Exception e)
         {
