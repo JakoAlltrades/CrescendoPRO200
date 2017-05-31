06580b1fdded3e304718e6065a635ac09d20c246
JakoAlltrades <Jpriemo@gmail.com>
2017-05-31 14:25:33 -0600

    DBHandler Pulling userbyID
    It is finally working and returns a user

--- CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/DBHandler.java
+++ CrescendoApp/app/src/main/java/crescendo/com/crescendoapp/DBHandler.java
@@ -1,10 +1,9 @@
 package crescendo.com.crescendoapp;
 
-import com.intuit.quickbase.api.QuickBase;
-import com.intuit.quickbase.api.QuickBaseAuthenticator;
-import com.intuit.quickbase.api.QuickBaseConnection;
 import com.intuit.quickbase.util.QuickBaseClient;
 
+import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 
 /**
@@ -12,32 +11,49 @@ import java.util.Vector;
  */
 
 public class DBHandler {
-    QuickBaseConnection QBC;
-    QuickBase QB;
-    QuickBaseAuthenticator QBA;
     QuickBaseClient QBClient;
 
     public DBHandler() {
-
-        //QBClient = new QuickBaseClient("Jpriemo1234@gmail.com", "Crescendo1", "https://johnpriem.quickbase.com/db/");
         QBClient = new QuickBaseClient("Jpriemo1234@gmail.com", "Crescendo1", "https://johnpriem.quickbase.com/db/");
-        QBClient.setAppToken("duzpt2fcvsybbgkrkup4bjurh8b");
+        //QBClient.setAppToken("duzpt2fcvsybbgkrkup4bjurh8b");
         GetsUserByID(1);
     }
 
-    public void GetsUserByID(int id)
+    public User GetsUserByID(int id)
     {
+        User user = null;
         try {
             //String tableID = QBClient.findDbByName("CrescendoDB");
             //Log.i("Table:", tableID);
-            Vector v = QBClient.doQuery("bms24ytdy", "{'0'.EX." + id + "}","0.1.2.3", "", "");
+            Vector v = QBClient.doQuery("bms24ytdy", "{'0'.EX." + id + "}","a", "", "");
             //QBClient.doQuery("bms24ytdy", "{'0'.EX." + id + "}","UserID.UserName.UserPassword", "", "");
-            Object o = v.get(0);
+            Map<String, String> o = (Map<String, String>) v.get(0);
+            Set<Map.Entry<String, String>> set = o.entrySet();
+            String uName = null, uPassword = null;
+            int userID;
+            for(Map.Entry<String, String> e: set)
+            {
+                if(e.getKey().equals("UserID"))
+                {
+                   userID = Integer.parseInt(e.getValue());
+                }
+                if(e.getKey().equals("UserName"))
+                {
+                    uName = e.getValue();
+                }
+                if(e.getKey().equals("UserPassword"))
+                {
+                    uPassword =  e.getValue();
+                }
+            }
+            user = new User(uName,uPassword);
+
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
+        return user;
     }
 
 
