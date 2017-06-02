e14bde3cccd7363e0e228f2e6c363815afd4dd53
Chance Hollis <chancehollis@gmail.com>
2017-06-02 13:14:02 -0600

    Working on showing chords in app

---
+++ CrescendoSite/CrescendoWebsite/Models/Metronome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Web;
+using System.Web.UI;
+
+namespace CrescendoWebsite.Models
+{
+    public class Metronome
+    {
+        public int BeatsPerMin { get; set; }
+        private const int minuteMilTime = 60000;
+        public SoundPlayer Player { get; set; }
+
+        public Metronome(SoundPlayer player, int bpm)
+        {
+            BeatsPerMin = bpm;
+            Player = player;
+            player.Load();
+        }
+        public int GetInterval()
+        {
+            return (minuteMilTime / BeatsPerMin);
+        }
+        public void MetronomeTick(object sender, EventArgs e)
+        {
+            Player.Play();
+        }
+    }
+}
